74748bb7b4ab2b0c11e7e0d69fa8add68be13751
Xoz99 <andrianadiwahyono2005@gmail.com>
2026-09-10 10:11:26 +0700

    fix maps

--- src/components/MapTracker.tsx
+++ src/components/MapTracker.tsx
@@ -325,8 +325,8 @@ export default function MapTracker({ bins, focus }: Props) {
         zoomControl={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         <MapController focus={focus ?? null} />
